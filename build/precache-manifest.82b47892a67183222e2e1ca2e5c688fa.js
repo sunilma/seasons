10ca12c14ff2c9192b1baf9847d69c8af1245a4e
Sunil Maharjan <ratnasunil70@gmail.com>
2020-03-06 12:21:01 +0200

    added to github pages

---
+++ build/precache-manifest.82b47892a67183222e2e1ca2e5c688fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19426df8c3fbe3bfa6f54abd02dc7d9a",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "2276a5f37530b72b0845",
+    "url": "/seasons/static/css/main.2bef5a67.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "2276a5f37530b72b0845",
+    "url": "/seasons/static/js/main.6020bc62.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  }
+]);
